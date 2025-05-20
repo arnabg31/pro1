a2d4223899fe54af04903d084ac8170b0d17e02b
itzjhuma <jhumabetal312@gmail.com>
2025-05-20 19:18:56 +0530

    slider image

---
+++ bestsallers.php
@@ -0,0 +1,55 @@
+<!DOCTYPE htlm>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>test</title>
+    <!--fav icons-->
+    <link rel="icon" href="image\logo.png" type="image/x-icon">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+<link href="css/min.css" rel="stylesheet">
+
+    <!-- JS -->
+    <script src="js/bootstrap.js"></script>
+
+    <!--google font-->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+
+    <!---Font awesome-->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+<script src="https://kit.fontawesome.com/a7612e0a07.js" crossorigin="anonymous"></script>
+
+
+    <!-- ********* Header *********-->
+
+
+<!-- Navigator bar -->
+
+    <div class="container-fluid bg-dark position-relative shadow ">
+             <nav class="navbar navbar-expand-lg navbar-dark py-5 py-lg-0 px-0  px-lg-5">
+            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-between"  id="navbarCollapse">
+                <div class="navbar-nav font-weight-bold mx-auto py-0 text-left">
+                    <a href="index.php"  class="nav-item nav-link active"><i class="fa-solid fa-house"></i>HOME </a>
+                    <a href="about.php"  class="nav-item nav-link active"><i class="fa-solid fa-address-card"></i> ABOUT US </a>
+                    <a href="best sallers.php" class="nav-item nav-link active"><i class="fa-solid fa-chess-board"></i> Best Sallers </a>
+                    <a href="gift ideas.php"  class="nav-item nav-link"><i class="fa-solid fa-gift"></i> Gift Ideas </a>
+                    <a href="today details.php"  class="nav-item nav-link"><i class="fa-solid fa-calendar-week"></i> Today Details</a>
+                    <a href="customer service.php"  class="nav-item nav-link"> <i class="fa-brands fa-aws"></i>  Customer Service </a>
+                    <a href="contact.php"  class="nav-item nav-link active"><i class="fa-regular fa-id-badge"></i> Contact </a>
+                </div>
+            </div>
+            </nav>
+
+        </div>
+
+         <!-- *************Footer***************-->
+
+<?php include ("footer.php");?>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -12,6 +12,8 @@
 
     <!-- JS -->
     <script src="js/bootstrap.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
     <!--google font-->
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
@@ -38,8 +40,8 @@
             <div class="collapse navbar-collapse justify-content-between"  id="navbarCollapse">
                 <div class="navbar-nav font-weight-bold mx-auto py-0 text-left">
                     <a href="index.php"  class="nav-item nav-link active"><i class="fa-solid fa-house"></i>HOME </a>
-                    <a href="about.php"  class="nav-item nav-link"><i class="fa-solid fa-address-card"></i> ABOUT US </a>
-                    <a href="best sallers.php" class="nav-item nav-link"><i class="fa-solid fa-chess-board"></i> Best Sallers </a>
+                    <a href="about.php"  class="nav-item nav-link active"><i class="fa-solid fa-address-card"></i> ABOUT US </a>
+                    <a href="best sallers.php" class="nav-item nav-link active"><i class="fa-solid fa-chess-board"></i> Best Sallers </a>
                     <a href="gift ideas.php"  class="nav-item nav-link"><i class="fa-solid fa-gift"></i> Gift Ideas </a>
                     <a href="today details.php"  class="nav-item nav-link"><i class="fa-solid fa-calendar-week"></i> Today Details</a>
                     <a href="customer service.php"  class="nav-item nav-link"> <i class="fa-brands fa-aws"></i>  Customer Service </a>
@@ -114,7 +116,7 @@ om/jquery-3.5.1.slim.min.js"></script>
 </div>-->
 
 
-<!-- Slider-->
+<!-- Slider
  <div class="carousel-inner">
       <div class="item active">
         <img src="image\ph.jpg"  alt="Los Angeles" style="width:100%;">
@@ -129,6 +131,57 @@ om/jquery-3.5.1.slim.min.js"></script>
       </div>
     </div>
 
+    
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>-->
+
+<div class="container">
+  <h2>Carousel Example</h2>
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+
+      <div class="item active">
+        <img src="mall.jpg" alt="Los Angeles" style="width:100%;">
+        <div class="carousel-caption">
+          <h3>Los Angeles</h3>
+          <p>LA is always so much fun!</p>
+        </div>
+      </div>
+
+      <div class="item">
+        <img src="man.jpg" alt="Chicago" style="width:100%;">
+        <div class="carousel-caption">
+          <h3>Chicago</h3>
+          <p>Thank you, Chicago!</p>
+        </div>
+      </div>
+    
+      <div class="item">
+        <img src="women.jpg" alt="New York" style="width:100%;">
+        <div class="carousel-caption">
+          <h3>New York</h3>
+          <p>We love the Big Apple!</p>
+        </div>
+      </div>
+  
+    </div>
+
     <!-- Left and right controls -->
     <a class="left carousel-control" href="#myCarousel" data-slide="prev">
       <span class="glyphicon glyphicon-chevron-left"></span>
@@ -140,6 +193,7 @@ om/jquery-3.5.1.slim.min.js"></script>
     </a>
   </div>
 </div>
+
 
 
        <!-- *************Footer***************-->
