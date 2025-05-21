be4f2284725ad6b6e1e5f0d9703c760d75ee4d47
itzjhuma <jhumabetal312@gmail.com>
2025-05-21 12:38:05 +0530

    slider img

--- index.php
+++ index.php
@@ -25,6 +25,19 @@
 
     <!-- ********* Header *********-->
 
+    <!-- Navigator bar -->
+
+    <div class="container-fluid bg-dark position-relative shadow ">
+             <nav class="navbar navbar-expand-lg navbar-dark py-5 py-lg-0 px-0 px-lg-5">
+            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-between"  id="navbarCollapse">
+                <div class="navbar-nav font-weight-bold mx-auto py-0 text-left">
+                    <a href="index.php"  class="nav-item nav-link active">WORLD WIDE SHIPPING Avaiable. </a>
+</nav>
+</div>
+
 
 <!-- Navigator bar -->
  <div class="container-fluid p-1 my-1 bg-teal text-blue">
@@ -93,105 +106,31 @@ om/jquery-3.5.1.slim.min.js"></script>
 
 <!--Slider Image-->
 
-    <!--<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
-    <img style="width: 1530px; height: 400px;" src="mall.jpg" alt="First slide">
+      <img class="d-block w-100 h-50" src="image/ph.jpg" alt="First slide">
     </div>
     <div class="carousel-item active">
-      <img style="width: 1530px; height: 450px;" src="women.jpg" alt="Second slide">
+      <img class="d-block w-100 h-50" src="image/pic9.jpg" alt="Second slide">
     </div>
     <div class="carousel-item active">
-      <img style="width: 1530px; height: 450px;" src="man.jpg" alt="Third slide">
+      <img class="d-block w-100 h-50" src="image/pic6.jpg" alt="Third slide">
     </div>
   </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
   </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
   </a>
-</div>-->
-
-
-<!-- Slider
- <div class="carousel-inner">
-      <div class="item active">
-        <img src="image\ph.jpg"  alt="Los Angeles" style="width:100%;">
-      </div>
-
-      <div class="item">
-        <img src="image\pic6.jpg" alt="Chicago" style="width:100%;">
-      </div>
-    
-      <div class="item">
-        <img src="image\pic9.jpg" alt="New york" style="width:100%;">
-      </div>
-    </div>
-
-    
-    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div>
-</div>-->
-
-<div class="container">
-  <h2>Carousel Example</h2>
-  <div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-      <li data-target="#myCarousel" data-slide-to="1"></li>
-      <li data-target="#myCarousel" data-slide-to="2"></li>
-    </ol>
-
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner">
-
-      <div class="item active">
-        <img src="mall.jpg" alt="Los Angeles" style="width:100%;">
-        <div class="carousel-caption">
-          <h3>Los Angeles</h3>
-          <p>LA is always so much fun!</p>
-        </div>
-      </div>
-
-      <div class="item">
-        <img src="man.jpg" alt="Chicago" style="width:100%;">
-        <div class="carousel-caption">
-          <h3>Chicago</h3>
-          <p>Thank you, Chicago!</p>
-        </div>
-      </div>
-    
-      <div class="item">
-        <img src="women.jpg" alt="New York" style="width:100%;">
-        <div class="carousel-caption">
-          <h3>New York</h3>
-          <p>We love the Big Apple!</p>
-        </div>
-      </div>
-  
-    </div>
-
-    <!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div>
 </div>
 
 
