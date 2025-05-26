f14bd529de58b0e1114d37469e8f777fc47ec86a
Arnab Ghosh <arnab712148@outlook.com>
2025-05-26 21:02:27 +0530

    nav bar modify

--- contact.php
+++ contact.php
@@ -36,9 +36,9 @@
             </button>
             <div class="collapse navbar-collapse justify-content-between"  id="navbarCollapse">
                 <div class="navbar-nav font-weight-bold mx-auto py-0 text-left">
-                    <a href="index.php"  class="nav-item nav-link active"><i class="fa-solid fa-house"></i>HOME </a>
-                    <a href="about.php"  class="nav-item nav-link active"><i class="fa-solid fa-address-card"></i> ABOUT US </a>
-                    <a href="best_sellers" class="nav-item nav-link active"><i class="fa-solid fa-chess-board"></i> Best Sellers </a>
+                    <a href="index.php"  class="nav-item nav-link "><i class="fa-solid fa-house"></i>HOME </a>
+                    <a href="about.php"  class="nav-item nav-link "><i class="fa-solid fa-address-card"></i> ABOUT US </a>
+                    <a href="bestsallers.php" class="nav-item nav-link "><i class="fa-solid fa-chess-board"></i> Best Sellers </a>
                     <a href="amount.php"  class="nav-item nav-link"> <i class="fa-brands fa-aws"></i>Amount </a>
                     <a href="contact.php"  class="nav-item nav-link active"><i class="fa-regular fa-id-badge"></i> Contact </a>
                 </div>
