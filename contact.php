4e538db453065b27d99500135f2b589b7f2fe1fd
itzjhuma <jhumabetal312@gmail.com>
2025-05-27 20:37:27 +0530

    contact mod2

--- contact.php
+++ contact.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>pro1</title>
-    <!--fav icons-->
-    <link rel="icon" href="image\logo.png" type="image/x-icon">
+     <!--fav icons-->
+    <link rel="icon" href="image\logo1.png" type="image/x-icon">
 
     <!-- CSS -->
     <link rel="stylesheet" href="css/bootstrap.css">
