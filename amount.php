eb7b692e2f3e045b9cfc280990eb085f2dba5224
itzjhuma <jhumabetal312@gmail.com>
2025-05-29 10:04:31 +0530

    amount mod

---
+++ amount.php
@@ -0,0 +1,319 @@
+<!DOCTYPE htlm>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pro1</title>
+    <!--fav icons-->
+    <link rel="icon" href="image\logo1.png" type="image/x-icon">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+   
+    <!-- JS -->
+    <script src="js/bootstrap.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+ 
+    <!--google font-->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+
+    <!---Font awesome-->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+<script src="https://kit.fontawesome.com/a7612e0a07.js" crossorigin="anonymous"></script>
+
+
+    <!-- ********* Header *********-->
+<!-- Navigator bar -->
+     <a class="navbar-brand" href="#">
+    <img src="image/fashionbeautylogo.png" width="120" height="130" class="d-inline-block align-top=22" alt="">
+    Fashion Beauty
+  </a>
+<section class="py-5">
+	<nav class="navbar bg-light">
+		<div class="container">
+			<form class="d-lg-flex d-none align-items-center position-relative">
+				<div class="input-group align-items-center">
+<input aria-describedby="button-addon2" aria-label="Search" class="form-control" placeholder="Search" type="text"> <button class="btn bg-white border" id="button-addon2" type="button"><svg class="bi bi-search" fill="currentColor" height="20" viewbox="0 0 10 16" width="20" xmlns="http://www.w3.org/2000/svg">
+				<path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"></path></svg></button>
+				</div>
+			</form>
+			<div>
+			<!--	<a class="navbar-brand" href=""><img alt="Free Frontend Logo" class="img-fluid" height="" src="https://freefrontend.dev/wp-content/uploads/free-frontend-logo.png" width="300"></a>-->
+			</div>
+			<div class="d-block">
+				<div class="d-flex ms-auto">
+					<a class="text-dark border border-2 border-dark rounded-circle d-flex align-items-center justify-content-center" href="" style="height:32px;width:32px;"><svg class="bi bi-facebook-f" fill="currentColor" height="16" viewbox="0 0 24 24" width="16" xmlns="http://www.w3.org/2000/svg">
+					<path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a> <a class="text-dark border border-2 border-dark rounded-circle d-flex align-items-center justify-content-center ms-2" href="" style="height:32px;width:32px;"><svg class="bi bi-twitter" fill="currentColor" height="16" viewbox="0 0 16 16" width="16" xmlns="http://www.w3.org/2000/svg">
+					<path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"></path></svg></a> <a class="text-dark border border-2 border-dark rounded-circle d-flex align-items-center justify-content-center ms-2" href="" style="height:32px;width:32px;"><svg class="bi bi-instagram" fill="currentColor" height="16" viewbox="0 0 16 16" width="16" xmlns="http://www.w3.org/2000/svg">
+					<path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"></path></svg></a>
+				</div>
+			</div>
+		</div>
+          </nav>
+    <div class="container-fluid bg-dark position-relative shadow ">
+             <nav class="navbar navbar-expand-lg navbar-dark py-5 py-lg-0 px-0 px-lg-5">
+            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-between"  id="navbarCollapse">
+                <div class="navbar-nav font-weight-bold mx-auto py-0 text-left">
+                    <a href="index.php"  class="nav-item nav-link active"><i class="fa-solid fa-house"></i>HOME </a>
+                    <a href="about.php"  class="nav-item nav-link "><i class="fa-solid fa-address-card"></i> ABOUT US </a>
+                    <a href="best sallers.php" class="nav-item nav-link "><i class="fa-solid fa-chess-board"></i> Best Sallers </a>
+                    <a href="amount.php"  class="nav-item nav-link active"> <i class="fa-solid fa-comments-dollar"></i> Amount </a>
+                    <a href="contact.php"  class="nav-item nav-link"><i class="fa-regular fa-id-badge"></i> Contact </a>
+
+                </div>
+            </div>
+            </nav>
+
+        </div>
+
+
+        
+    <!--<div class="row mb-4">
+        <div class="col-lg-8 mx-auto text-center">
+            <h1 class="display-6">Bootstrap Payment Forms</h1>
+        </div>
+    </div> 
+    <div class="row">
+        <div class="col-lg-6 mx-auto">
+            <div class="card ">
+                <div class="card-header">
+                    <div class="bg-white shadow-sm pt-4 pl-2 pr-2 pb-2">
+                       
+                        <ul role="tablist" class="nav bg-light nav-pills rounded nav-fill mb-3">
+                            <li class="nav-item"> <a data-toggle="pill" href="#credit-card" class="nav-link active "> <i class="fas fa-credit-card mr-2"></i> Credit Card </a> </li>
+                            <li class="nav-item"> <a data-toggle="pill" href="#paypal" class="nav-link "> <i class="fab fa-paypal mr-2"></i> Paypal </a> </li>
+                            <li class="nav-item"> <a data-toggle="pill" href="#net-banking" class="nav-link "> <i class="fas fa-mobile-alt mr-2"></i> Net Banking </a> </li>
+                        </ul>
+                    </div> 
+                    <div class="tab-content">
+                        
+                        <div id="credit-card" class="tab-pane fade show active pt-3">
+                            <form role="form" onsubmit="event.preventDefault()">
+                                <div class="form-group"> <label for="username">
+                                        <h6>Card Owner</h6>
+                                    </label> <input type="text" name="username" placeholder="Card Owner Name" required class="form-control "> </div>
+                                <div class="form-group"> <label for="cardNumber">
+                                        <h6>Card number</h6>
+                                    </label>
+                                    <div class="input-group"> <input type="text" name="cardNumber" placeholder="Valid card number" class="form-control " required>
+                                        <div class="input-group-append"> <span class="input-group-text text-muted"> <i class="fab fa-cc-visa mx-1"></i> <i class="fab fa-cc-mastercard mx-1"></i> <i class="fab fa-cc-amex mx-1"></i> </span> </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-sm-8">
+                                        <div class="form-group"> <label><span class="hidden-xs">
+                                                    <h6>Expiration Date</h6>
+                                                </span></label>
+                                            <div class="input-group"> <input type="number" placeholder="MM" name="" class="form-control" required> <input type="number" placeholder="YY" name="" class="form-control" required> </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-4">
+                                        <div class="form-group mb-4"> <label data-toggle="tooltip" title="Three digit CV code on the back of your card">
+                                                <h6>CVV <i class="fa fa-question-circle d-inline"></i></h6>
+                                            </label> <input type="text" required class="form-control"> </div>
+                                    </div>
+                                </div>
+                                <div class="card-footer"> <button type="button" class="subscribe btn btn-primary btn-block shadow-sm"> Confirm Payment </button>
+                            </form>
+                        </div>
+                    </div> 
+                    <div id="paypal" class="tab-pane fade pt-3">
+                        <h6 class="pb-2">Select your paypal account type</h6>
+                        <div class="form-group "> <label class="radio-inline"> <input type="radio" name="optradio" checked> Domestic </label> <label class="radio-inline"> <input type="radio" name="optradio" class="ml-5">International </label></div>
+                        <p> <button type="button" class="btn btn-primary "><i class="fab fa-paypal mr-2"></i> Log into my Paypal</button> </p>
+                        <p class="text-muted"> Note: After clicking on the button, you will be directed to a secure gateway for payment. After completing the payment process, you will be redirected back to the website to view details of your order. </p>
+                    </div> 
+                   
+                    <div id="net-banking" class="tab-pane fade pt-3">
+                        <div class="form-group "> <label for="Select Your Bank">
+                                <h6>Select your Bank</h6>
+                            </label> <select class="form-control" id="ccmonth">
+                                <option value="" selected disabled>--Please select your Bank--</option>
+                                <option>Bank 1</option>
+                                <option>Bank 2</option>
+                                <option>Bank 3</option>
+                                <option>Bank 4</option>
+                                <option>Bank 5</option>
+                                <option>Bank 6</option>
+                                <option>Bank 7</option>
+                                <option>Bank 8</option>
+                                <option>Bank 9</option>
+                                <option>Bank 10</option>
+                            </select> </div>
+                        <div class="form-group">
+                            <p> <button type="button" class="btn btn-primary "><i class="fas fa-mobile-alt mr-2"></i> Proceed Payment</button> </p>
+                        </div>
+                        <p class="text-muted">Note: After clicking on the button, you will be directed to a secure gateway for payment. After completing the payment process, you will be redirected back to the website to view details of your order. </p>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>-->
+
+   <!-- <div class="padding">
+<div class="row">
+<div class="col-sm-6">
+<div class="card">
+<div class="card-header">
+<strong>Credit Card</strong>
+<small>enter your card details</small>
+</div>
+<div class="card-body">
+<div class="row">
+<div class="col-sm-12">
+<div class="form-group">
+    
+
+    
+<label for="name">Name</label>
+<input class="form-control" id="name" type="text" placeholder="Enter your name">
+</div>
+</div>
+</div>
+
+<div class="row">
+<div class="col-sm-12">
+<div class="form-group">
+<label for="ccnumber">Credit Card Number</label>
+<div class="input-group">
+<input class="form-control" type="text" placeholder="0000 0000 0000 0000" autocomplete="email">
+<div class="input-group-append">
+<span class="input-group-text">
+<i class="mdi mdi-credit-card"></i>
+</span>
+</div>
+</div> 
+</div>
+</div>
+</div>
+
+<div class="row">
+<div class="form-group col-sm-4">
+<label for="ccmonth">Month</label>
+<select class="form-control" id="ccmonth">
+<option>1</option>
+<option>2</option>
+<option>3</option>
+<option>4</option>
+</select>
+</div>
+<div class="form-group col-sm-4">
+<label for="ccyear">Year</label>
+<select class="form-control" id="ccyear">
+<option>2014</option>
+<option>2015</option>
+<option>2016</option>
+<option>2017</option>
+<option>2018</option>
+<option>2019</option>
+<option>2020</option>
+<option>2021</option>
+<option>2022</option>
+<option>2023</option>
+<option>2024</option>
+<option>2025</option>
+</select>
+</div>
+<div class="col-sm-4">
+<div class="form-group">
+<label for="cvv">CVV/CVC</label>
+<input class="form-control" id="cvv" type="text" placeholder="123">
+</div>
+</div>
+</div>
+
+</div>
+<div class="card-footer">
+<button class="btn btn-sm btn-success float-right" type="submit">
+<i class="mdi mdi-gamepad-circle"></i> Continue</button>
+<button class="btn btn-sm btn-danger" type="reset">
+<i class="mdi mdi-lock-reset"></i> Reset</button>
+</div>
+</div>
+</div>
+</div>
+</div>-->
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.payment/3.0.0/jquery.payment.min.js"></script>
+ <div class="padding">
+        <div class="row">
+            <div class="container-fluid d-flex justify-content-center">
+                <div class="col-sm-8 col-md-6">
+                    <div class="card">
+                        <div class="card-header">
+
+                        <div class="row">
+                            <div class="col-md-6">
+                                <span>CREDIT/DEBIT CARD PAYMENT</span>
+                                
+                            </div>
+
+                            <div class="col-md-6 text-right" style="margin-top: -5px;">
+
+                                  <img src="https://img.icons8.com/color/36/000000/visa.png">
+                                  <img src="https://img.icons8.com/color/36/000000/mastercard.png">
+                                  <img src="https://img.icons8.com/color/36/000000/amex.png">
+                                           
+                            </div>      
+
+                        </div>    
+  
+                        </div>
+                        <div class="card-body" style="height: 350px">
+                            <div class="form-group">
+                            <label for="cc-number" class="control-label">CARD NUMBER</label>
+                            <input id="cc-number" type="tel" class="input-lg form-control cc-number" autocomplete="cc-number" placeholder="&bull;&bull;&bull;&bull; &bull;&bull;&bull;&bull; &bull;&bull;&bull;&bull; &bull;&bull;&bull;&bull;" required>
+                          </div>
+
+                          <div class="row">
+
+                            <div class="col-md-6">
+                                 <div class="form-group">
+                                    <label for="cc-exp" class="control-label">CARD EXPIRY</label>
+                                    <input id="cc-exp" type="tel" class="input-lg form-control cc-exp" autocomplete="cc-exp" placeholder="&bull;&bull; / &bull;&bull;" required>
+                                  </div>
+
+                                
+                            </div>
+
+                             <div class="col-md-6">
+                               <div class="form-group">
+                                <label for="cc-cvc" class="control-label">CARD CVC</label>
+                                <input id="cc-cvc" type="tel" class="input-lg form-control cc-cvc" autocomplete="off" placeholder="&bull;&bull;&bull;&bull;" required>
+                              </div>
+                            </div>
+                              
+                          </div>
+
+      
+                          <div class="form-group">
+                            <label for="numeric" class="control-label">CARD HOLDER NAME</label>
+                            <input  type="text" class="input-lg form-control">
+                          </div>
+
+                           <div class="form-group">
+                            
+                            <input  value="MAKE PAYMENT" type="button" class="btn btn-success btn-lg form-control" style="font-size: .8rem;">
+                          </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+</div>
+    
+
+        <!-- *************Footer***************-->
+
+<?php include ("footer.php");?>
+                    </body>
+                    </html>
