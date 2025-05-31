8fde1414281b10b31cdd8da7cb1d7e654e1b9e48
itzjhuma <jhumabetal312@gmail.com>
2025-05-31 09:38:44 +0530

    bestsallers mod1

--- about.php
+++ about.php
@@ -118,43 +118,9 @@ Our team at MarisGold Unisex Boutique is dedicated to providing a personalized s
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.4.4/dist/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/js/all.min.js"></script>
 
- <!-- <section class="py-5">
-	<div class="container">
-		<div class="row gx-4 align-items-center justify-content-between">
-			<div class="col-md-5 order-2 order-md-1">
-				<div class="mt-5 mt-md-0">
-					<span class="text-muted">Our Story</span>
-					<h2 class="display-5 fw-bold">About Us</h2>
-					<p class="lead">Since 2014, MarisGold Unisex Boutique has been a beacon of style, inclusivity, and innovation in the world of fashion. With a legacy of lengthy years in the industry, we have redefined the concept of unisex clothing, offering a diverse range of apparels and Accesories that transcend gender norms and celebrate individuality.
-
-Our boutique is a curated haven for those who appreciate quality craftsmanship, timeless designs, and a touch of sophistication in every piece. Whether you're looking for sleek streetwear, chic casual wear, or elegant formal attire, MarisGold Unisex Boutique has something for everyone. Each item in our boutique is carefully selected to embody a perfect blend of sophistication, comfort, and versatility, catering to the diverse tastes and preferences of our discerning clientele.
-
-Our team at MarisGold Unisex Boutique is dedicated to providing a personalized shopping experience, where every visit leaves you feeling inspired and empowered to embrace your unique style journey. Whether you're exploring our collections in-store or browsing online, our friendly staff are here to assist you in finding the perfect pieces that speak to your individuality.
-</p>
-				</div>
-			</div>
-			<div class="col-md-6 offset-md-1 order-1 order-md-2">
-				<div class="row gx-2 gx-lg-3">
-					<div class="col-6">
-						<div class="mb-2"><img class="img-fluid rounded-3" src="image/saree3.jpg"></div>
-					</div>
-					<div class="col-6">
-						<div class="mb-2"><img class="img-fluid rounded-3" src="image/sariw.jpg"></div>
-					</div>
-					<div class="col-6">
-						<div class="mb-2"><img class="img-fluid rounded-3" src="image/saree1.jpg"></div>
-					</div>
-					<div class="col-6">
-						<div class="mb-2"><img class="img-fluid rounded-3" src="image/Artboard.jpg"></div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
-
-<!- *************Footer***************-->
+<!- -*************Footer***************->
 
 <?php include ("footer.php");?>
 </body>
--- bestsallers.php
+++ bestsallers.php
@@ -71,6 +71,52 @@
             </nav>
 
         </div>
+
+
+    <section class="py-5">
+	<div class="container">
+		<div class="row justify-content-center text-center mb-2 mb-lg-4">
+			<div class="col-12 col-lg-8 col-xxl-7 text-center mx-auto">
+				<span class="text-muted">Showcase</span>
+				<h2 class="display-5 fw-bold">Our Gallery</h2>
+				<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta harum ipsum venenatis metus sem veniam eveniet aperiam suscipit.</p>
+			</div>
+		</div>
+		<div class="row py-3 align-items-center">
+			<div class="col-md-6 mt-md-0 mt-4">
+				<div class="mb-5 mb-lg-3"><img class="img-fluid" src="https://freefrontend.dev/assets/rectangle-wide.png"></div>
+			</div>
+			<div class="col-md-6 ps-md-5">
+				<div class="mb-5 mb-lg-3">
+					<h4>A Catchy Title</h4>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente accusantium nulla, sequi cupiditate exercitationem sit incidunt rerum explicabo. Soluta beatae esse praesentium exercitationem vero harum pariatur adipisci qui veritatis est!</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta harum ipsum venenatis metus sem veniam eveniet aperiam suscipit.</p><a class="btn btn-lg btn-primary" href="">View Gallery</a>
+				</div>
+			</div>
+		</div>
+		<div class="row mt-2">
+			<div class="col-lg-3 col-md-6">
+				<div class="mb-3 mb-lg-0"><img alt="" class="img-fluid" src="https://freefrontend.dev/assets/rectangle-wide.png"></div>
+			</div>
+			<div class="col-lg-3 col-md-6">
+				<div class="mb-3 mb-lg-0"><img alt="" class="img-fluid" src="https://freefrontend.dev/assets/rectangle-wide.png"></div>
+			</div>
+			<div class="col-lg-3 col-md-6">
+				<div class="mb-3 mb-lg-0"><img alt="" class="img-fluid" src="https://freefrontend.dev/assets/rectangle-wide.png"></div>
+			</div>
+			<div class="col-lg-3 col-md-6">
+				<div class="mb-3 mb-lg-0"><img alt="" class="img-fluid" src="https://freefrontend.dev/assets/rectangle-wide.png"></div>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+					
+
+
+
+
          <!-- *************Footer***************-->
 
 <?php include ("footer.php");?>
