49d1a258ffc108c3babb4950d8de83164b41f138
itzjhuma <jhumabetal312@gmail.com>
2025-06-15 09:08:22 +0530

    lernmore mod

--- lernmore.php
+++ lernmore.php
@@ -1,33 +1,13 @@
-<!--<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Bootstrap Example</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-</head>
-<body>
-
-<div class="container">
-  <h3>Popover Example</h3>
-    <a href="#" title="Dismissible popover" data-toggle="popover" data-trigger="focus" data-content="Click anywhere in the document to close this popover">Click me</a>
-</div>
-
-<script>
-$(document).ready(function(){
-  $('[data-toggle="popover"]').popover();   
-});
-</script>
-
-</body>
-</html>-->
-
+// Description: This code creates a simple HTML page with two buttons that open modals with information about "Fast Delivery" and "Exchange Offer".
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Bootstrap Example</title>
+  <title>pro1</title>
+   <!--fav icons-->
+    <link rel="icon" href="image\logo1.png" type="image/x-icon">
+  <link rel="shortcut icon" href="image\logo1.png" type="image/x-icon">
+
+  <!-- Bootstrap CSS and JS -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
@@ -36,7 +16,7 @@ $(document).ready(function(){
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
-
+<!-- Fast Delivery -->
 <div class="container">
   <h2>Fast Delivery</h2>
   <!-- Button to Open the Modal -->
@@ -48,25 +28,8 @@ $(document).ready(function(){
   <div class="modal fade" id="myModal">
     <div class="modal-dialog modal-sm">
       <div class="modal-content">
-        <p class="text-center">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id eum magnam temporibus a sed consequuntur praesentium labore inventore quisquam asperiores, incidunt numquam. Libero aspernatur cum cumque quae modi! Voluptate, ratione.</p>
-        <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
-        <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
-        <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+        <p class="text-center text-success">Online shopping from a great selection at Fast Delivery Store ... End of dialog window. See Similar. Fast Delivery. Reviews. Brands. Price.</p>
 </div>
-
-
-      
-        <!-- Modal Header -->
-        <!--<div class="modal-header">
-          <h4 class="modal-title">Modal Heading</h4>
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-        </div>-->
-        
-        <!-- Modal body -->
-       <!-- <div class="modal-body">
-          Modal body..
-        </div>-->
-        
         <!-- Modal footer -->
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -77,6 +40,34 @@ $(document).ready(function(){
   </div>
   
 </div>
+
+
+
+<!-- Exchange Offer -->
+<div class="container">
+  <h2>Exchange Offer</h2>
+  <!-- Button to Open the Modal -->
+  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal1">
+    Lern More
+  </button>
+
+  <!-- The Modal -->
+  <div class="modal fade" id="myModal1">
+    <div class="modal-dialog modal-sm">
+      <div class="modal-content">
+        <p class="text-center text-success">Online shopping from a great selection at Exchange Offer Store ... End of dialog window. See Similar. Exchange Offer. Reviews. Brands. Price.</p>
+        <!-- Modal footer -->
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+  
+</div>
+
+
 
 </body>
 </html>
