454949f699afadbea43a1e47fa27999db7a1f408
itzjhuma <jhumabetal312@gmail.com>
2025-06-19 09:29:43 +0530

    index color

--- index.php
+++ index.php
@@ -68,7 +68,7 @@
 					</nav>
 
 
-    <div class="container-fluid bg-warning   position-relative shadow ">
+    <div class="container-fluid bg-info  position-relative shadow ">
              <nav class="navbar navbar-expand-lg navbar-dark py-5 py-lg-0 px-0  px-lg-5">
             <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
                 <span class="navbar-toggler-icon"></span>
