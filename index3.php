802b1f66b0843da0d16a4eccb41988f2a6cb96d8
Arnabg31 <arnab712148@outlook.com>
2025-08-25 16:24:47 +0530

    NEW PAGE

---
+++ index3.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>PHP Test</title>
+</head>                             
+<body>
+<?php
+echo "Hello World! This is index3.php";
+?>
+</body>
+</html>     
+        
